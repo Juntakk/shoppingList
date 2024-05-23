766b2b9d5ef56fee6cb3a46bc2ced51e2939219a
Juntakk <nick.habashi@gmail.com>
2024-05-23 13:32:12 -0400

    First commit

---
+++ lib/models/category.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  sweets,
+  spices,
+  dairy,
+  convenience,
+  hygiene,
+  other,
+  carbs,
+}
+
+class Category {
+  const Category(this.title, this.color);
+
+  final String title;
+  final Color color;
+}
